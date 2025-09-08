9c38d5576122f10e4de73aa290311b60e9daf32f
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-09-08 12:06:45 +0100

    fix : add index.js file

---
+++ index.js
@@ -0,0 +1,18 @@
+// var nom = 'papillon';
+// var age = 30;
+// var isStudent = false;
+// var students = ['Aaron', 'Dieudonné', 'Charlie'];
+// students.push('David');
+// var hobbies = ['reading', 'gaming', 'coding'];
+// hobbies.push('travelling');
+// function isMature(name, age) {
+//     if (age >= 18) {
+//         console.log(name + " is an adult.");
+//     }
+//     else {
+//         console.log(name + " is not an adult.");
+//     }
+// }
+// isMature('papillon', 15);
+// console.log(hobbies);
+// console.log(students);
